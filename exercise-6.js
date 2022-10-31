5f211ec9dc169496d281c2c915169ac6ab442ae0
Chiara Arrivas <ritarrivas96@gmail.com>
2022-10-31 15:26:39 +0100

    istanceof Operator

---
+++ exercise-6.js
@@ -0,0 +1,36 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(el) {
+    if (el instanceof Square) {
+      return el.side * el.side
+    } else if (el instanceof Rectangle) {
+      return el.width * el.height
+    } else { return Math.PI * (el.radius + el.radius) }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
